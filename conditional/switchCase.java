37c473fff9dda1605ba929de337a2af3f47ddb06
Khushhal Garg <khusgarg1@gmail.com>
2023-10-02 12:11:12 +0530

    Switch Case

---
+++ conditional/switchCase.java
@@ -0,0 +1,30 @@
+package conditional;
+
+import java.util.Scanner;
+
+public class switchCase {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        int num = in.nextInt();
+
+        switch (num) {
+            case 1:
+                System.out.println("Day1");
+                break;
+            case 2:
+                System.out.println("Day2");
+                break;
+            case 3:
+                System.out.println("Day3");
+                break;
+            case 4:
+                System.out.println("Day4");
+                break;
+
+            default:
+                System.out.println("Completed");
+                break;
+        }
+    }
+}
